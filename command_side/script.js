5e044c4f55d8fb3ed2837474fda9547acfdf4036
MujoBabajic <mbabajic.babajic56@gmail.com>
2025-01-28 01:44:19 +0100

    added data sending to firebase

--- command_side/script.js
+++ command_side/script.js
@@ -1,29 +1,40 @@
-const esp32IP = "192.168.5.1";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+import {firebaseConfig} from "./firebase_config.js";
 
-document.getElementById("sendBtn").addEventListener("click", sendMessage);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
 
-function sendMessage() {
-    const message = document.getElementById('textInput').value;
-    if (!message) {
-      alert('Please enter a message.');
-      return;
-    }
+function insertData() {
+  // Creating a timestamp for the message
+  const date = new Date();
+  const timestamp = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(
+    2,
+    "0"
+  )}${String(date.getDate()).padStart(2, "0")}${String(date.getHours()).padStart(
+    2,
+    "0"
+  )}${String(date.getMinutes()).padStart(2, "0")}${String(
+    date.getSeconds()
+  ).padStart(2, "0")}`;
 
-    fetch(`http://${esp32IP}/receive`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ message: message }),
+  // Sending the timestamp and message to Firebase
+  set(ref(db, "MessageLog" + timestamp), {
+    timestamp: timestamp,
+    message: textInput.value,
+  })
+    .then(() => {
+      alert("Data sent successfully");
     })
-      .then(response => {
-        if (response.ok) {
-          alert('Message sent successfully!');
-        } else {
-          alert('Failed to send the message.');
-        }
-      })
-      .catch(error => {
-        alert('Error: ' + error.message);
-      });
-  }
+    .catch((error) => {
+      console.log(error);
+      alert("Error: ", error);
+    });
+}
+
+sendBtn.addEventListener("click", insertData);
